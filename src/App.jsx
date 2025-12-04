f297115de2fb31f3ecdce8ae87adcdcaff4be486
kevin <kevinsamir1z32@gmail.com>
2025-12-04 17:58:40 +0200

    feat : added basic tanstack query setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
 
 import Auth from "./pages/Login";
 import Error from "./pages/Error";
@@ -14,6 +15,8 @@ import CallHistory from "./pages/Dashboard/CallHistory";
 import Analytics from "./pages/Dashboard/Analytics";
 import Settings from "./pages/Dashboard/Settings";
 import LiveCalls from "./pages/Dashboard/LiveCalls";
+
+const queryClient = new QueryClient(); // Create a client
 
 const router = createBrowserRouter([
   {
@@ -77,7 +80,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
