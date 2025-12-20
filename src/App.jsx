452ee688112d0a988c3a4e9f2cfbd74beb544dc4
MustafaAFarag <mustafa.ashraf.saad@gmail.com>
2025-12-20 22:28:58 +0200

    Fix Pricing Component Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
 
 import Auth from "./pages/Login";
 import Error from "./pages/Error";
@@ -16,6 +14,7 @@ import Analytics from "./pages/Dashboard/Analytics";
 import Settings from "./pages/Dashboard/Settings";
 import LiveCalls from "./pages/Dashboard/LiveCalls";
 import MainOnboarding from "./pages/Onboarding/MainOnBoarding";
+import Pricing from "./pages/Pricing";
 
 const queryClient = new QueryClient();
 
